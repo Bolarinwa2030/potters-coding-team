a3c23ec44d8278d8e2786933cbcde9eabbaae2c2
adediji704 <adediji704@gmail.com>
2026-06-27 11:17:19 +0100

    Navbar fixed from Deju

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav className='bg-red-600 text-white py-5 px-'>
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">POTTERS CODING TEAM</h1>
+        <ul className="hidden md:flex gap-10 font-semibold">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About Us</li>
+          <li className="cursor-pointer">Our Team</li>
+          <li className="cursor-pointer">Projects</li>
+        </ul>
+        
+        <button className="bg-white text-red-600 px-8 py-3 rounded-xl font-semibold">Join Us</button>
+      </div>
+    </nav>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwind from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
