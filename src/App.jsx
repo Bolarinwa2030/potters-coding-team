058cdd0c5d3048a96f6ec308dfbf8c07b8178656
Bolarinwa2030 <hablaw30@gmail.com>
2026-07-09 19:55:39 -0700

    Fix: Navbar error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Hero/>
+      <NavBar />
+      <Hero />
       <AboutUs />
-
     </div>
   );
 }
